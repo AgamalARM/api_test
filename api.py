b4018b6310966d2429a0d5b7c400324739ff56fa
AgamalARM <75130776+AgamalARM@users.noreply.github.com>
2022-05-07 00:13:44 +0200

    Update api.py

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ import time
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def home_page():
     data_set = {'page': 'Home',
                 'message': 'Success loaded Home page', 'Timestamp': time.time()}
@@ -22,18 +22,3 @@ def home_page():
     return json_dump
 
 
-@app.route('/user/', methods=['GET'])
-def request_page():
-    # 127.0.0.1:port/user/?user=USER_NAME
-    user_query = str(request.args.get('user'))
-    data_set = {'page': 'Request',
-                'message': f'Success got the request for{user_query}', 'Timestamp': time.time()}
-    json_dump = json.dumps(data_set)
-
-    return json_dump
-
-
-# if __name__ == '__main__':
-#     app.run(port=7777)
-
-home_page()
